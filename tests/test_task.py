8e12ab6cd28b17eee7945a343d249258cfd8f172
Thomas Stephens <thomas@ustudio.com>
2013-06-11 10:58:53 -0500

    Explicitly checking that task's properties are not writeable (because it is meant to be a read-only view of a task)

--- tests/test_task.py
+++ tests/test_task.py
@@ -4,6 +4,19 @@ import mock
 import unittest
 
 
+FULL_TASK_DATA = {
+    "status": "complete",
+    "queue": "some.queue",
+    "parameters": "some parameters",
+    "result": "a result",
+    "error": "no error",
+    "node": "some_node",
+    "pid": 1234,
+    "created": 1234.5,
+    "updated": 4567.89
+}
+
+
 class TestTask(unittest.TestCase):
     @mock.patch("redis.StrictRedis", autospec=True)
     def setUp(self, redis_class):
@@ -12,17 +25,7 @@ class TestTask(unittest.TestCase):
         self.client = Client("asdf", 1234, 0)
 
     def test_can_get_task_with_all_attributes(self):
-        self.mock_redis.hgetall.return_value = {
-            "status": "complete",
-            "queue": "some.queue",
-            "parameters": "some parameters",
-            "result": "a result",
-            "error": "no error",
-            "node": "some_node",
-            "pid": 1234,
-            "created": 1234.5,
-            "updated": 4567.89
-        }
+        self.mock_redis.hgetall.return_value = FULL_TASK_DATA
 
         task = self.client.get_task("some_task")
 
@@ -39,3 +42,38 @@ class TestTask(unittest.TestCase):
         self.assertEqual(1234, task.pid)
         self.assertEqual(1234.5, task.created)
         self.assertEqual(4567.89, task.updated)
+
+    def test_cannot_set_properties(self):
+        self.mock_redis.hgetall.return_value = FULL_TASK_DATA
+
+        task = self.client.get_task("some_task")
+
+        with self.assertRaises(AttributeError):
+            task.id = "foo"
+
+        with self.assertRaises(AttributeError):
+            task.status = "foo"
+
+        with self.assertRaises(AttributeError):
+            task.queue = "foo"
+
+        with self.assertRaises(AttributeError):
+            task.parameters = "foo"
+
+        with self.assertRaises(AttributeError):
+            task.result = "foo"
+
+        with self.assertRaises(AttributeError):
+            task.error = "foo"
+
+        with self.assertRaises(AttributeError):
+            task.node = "foo"
+
+        with self.assertRaises(AttributeError):
+            task.pid = 4321
+
+        with self.assertRaises(AttributeError):
+            task.created = 1.2
+
+        with self.assertRaises(AttributeError):
+            task.updated = 2.3
