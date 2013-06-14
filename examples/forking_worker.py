436c9859eb26829738b5abc46286f7cb7d22d3fe
Thomas Stephens <thomas@ustudio.com>
2013-06-14 15:38:20 -0500

    Updating forking worker example to use the built in version from the library

--- examples/forking_worker.py
+++ examples/forking_worker.py
@@ -2,69 +2,20 @@
 # figure out what API we want
 
 from blueque import Client
+from blueque import forking_runner
 
-import json
-import os
-import threading
 import time
 
 
-def do_work():
+def do_work(parameters):
+    print parameters
+
     time.sleep(1000)
 
-
-class WorkerThread(threading.Thread):
-    def __init__(self, client):
-        self._client = client
-        self.running = threading.Event()
-
-    def fork_task(self, task):
-        pid = os.fork()
-
-        if pid > 0:
-            return pid
-
-        processor = self._client.get_processor(task)
-
-        processor.start(os.getpid())
-
-        try:
-            result = do_work(json.loads(task.parameters))
-            processor.complete(json.dumps(result))
-        except Exception as e:
-            processor.fail(str(e))
-        finally:
-            os._exit(0)
-
-    def run(self):
-        self.running.set()
-
-        listener = self._client.get_listener()
-
-        while True:
-            task = listener.listen()
-
-            pid = self.fork_task(task)
-
-            pid, result = os.waitpid(pid, 0)
-            # should check result and clean up if process died abnormally.
+    return "result"
 
 
 if __name__ == "__main__":
-    concurrency = 4
-    threads = []
+    client = Client(hostname="localhost", port=6379, db=0)
 
-    client = Client()
-
-    while True:
-        for thread in threads:
-            if not thread.is_alive():
-                threads.remove(thread)
-
-        while len(threads) < concurrency:
-            thread = WorkerThread(client)
-            thread.start()
-
-            thread.running.wait()
-
-            threads.append(thread)
+    forking_runner.run(client, "some.queue", do_work, 4)
