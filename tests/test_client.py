d3ad0ecdf1569cbce69e7d88546b9902220c2649
Phred <fearphage@gmail.com>
2017-04-05 16:43:49 -0500

    pass additional params to redis constructor

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,9 +9,14 @@ class TestClient(unittest.TestCase):
     # classes they create.
 
     @mock.patch("redis.StrictRedis", autospec=True)
-    def setUp(self, mock_redis):
+    def test_client_connects_with_requested_information(self, mock_redis):
         self.client = Client(hostname="foo", port=1234, db=0)
-        self.mock_redis = mock_redis
 
-    def test_client_connects_with_requested_information(self):
-        self.mock_redis.assert_called_with(host="foo", port=1234, db=0)
+        mock_redis.assert_called_with(host="foo", port=1234, db=0)
+
+    @mock.patch("redis.StrictRedis", autospec=True)
+    def test_client_passes_additional_args_to_client(self, mock_redis):
+        self.client = Client(hostname="foo", port=1234, db=0, password="password", charset="utf8")
+
+        mock_redis.assert_called_with(
+            host="foo", port=1234, db=0, password="password", charset="utf8")
