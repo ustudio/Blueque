6940a5e0cdbbfbcac61dc2f68332399007a0cd7f
Tim Jensen <tjensen@ustudio.com>
2019-07-31 10:52:37 -0500

    Added Flake8 for linting and fixed linter errors. Also, removed `coverage` because it isn't being used.

--- examples/forking_worker.py
+++ examples/forking_worker.py
@@ -1,6 +1,7 @@
 # This hasn't actually been run; it's just a scratch-pad, for now, to
 # figure out what API we want
 
+from __future__ import print_function
 from blueque import Client
 from blueque import forking_runner
 
@@ -8,7 +9,7 @@ import time
 
 
 def do_work(task):
-    print task.id, task.parameters
+    print(task.id, task.parameters)
 
     time.sleep(1000)
 
