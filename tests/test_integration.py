8837db95664d68ffec6d97c71c3392fa95cbcb29
Tim Jensen <tjensen@ustudio.com>
2023-11-30 14:05:50 -0600

    Adding integration test that covers a task returning failed status
    Co-authored-by: Thomas Stephens <thomas@ustudio.com>

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -13,38 +13,56 @@ class TestIntegration(TestCase):
         redis_client = redis.StrictRedis.from_url(os.environ["REDIS_URI"])
         redis_client.flushdb()
 
+        self.producer_client = blueque.Client(os.environ["REDIS_URI"])
+        self.producer_queue = self.producer_client.get_queue("QUEUE-NAME")
+
+        self.worker_client = blueque.Client(os.environ["REDIS_URI"])
+        self.worker_listener = self.worker_client.get_listener("QUEUE-NAME")
+
     def test_task_can_be_enqueued_and_return_result(self):
-        producer_client = blueque.Client(os.environ["REDIS_URI"])
-        producer_queue = producer_client.get_queue("QUEUE-NAME")
+        task_id = self.producer_queue.enqueue("PARAMETERS")
 
-        worker_client = blueque.Client(os.environ["REDIS_URI"])
-        worker_listener = worker_client.get_listener("QUEUE-NAME")
+        self.assertEqual("pending", self.producer_client.get_task(task_id).status)
 
-        task_id = producer_queue.enqueue("PARAMETERS")
-
-        self.assertEqual("pending", producer_client.get_task(task_id).status)
-
-        task = worker_listener.listen()
+        task = self.worker_listener.listen()
 
         self.assertEqual(task_id, task.id)
         self.assertEqual("PARAMETERS", task.parameters)
 
-        self.assertEqual("reserved", producer_client.get_task(task_id).status)
+        self.assertEqual("reserved", self.producer_client.get_task(task_id).status)
         self.assertEqual(
-            f"{socket.getfqdn()}_{os.getpid()}", producer_client.get_task(task_id).node)
+            f"{socket.getfqdn()}_{os.getpid()}", self.producer_client.get_task(task_id).node)
 
-        processor = worker_client.get_processor(task)
+        processor = self.worker_client.get_processor(task)
 
         processor.start(123456)
 
-        self.assertEqual("started", producer_client.get_task(task_id).status)
-        self.assertEqual(123456, producer_client.get_task(task_id).pid)
+        self.assertEqual("started", self.producer_client.get_task(task_id).status)
+        self.assertEqual(123456, self.producer_client.get_task(task_id).pid)
 
         processor.complete("RESULT")
 
-        self.assertEqual("complete", producer_client.get_task(task_id).status)
-        self.assertEqual("RESULT", producer_client.get_task(task_id).result)
+        self.assertEqual("complete", self.producer_client.get_task(task_id).status)
+        self.assertEqual("RESULT", self.producer_client.get_task(task_id).result)
+        self.assertIsNone(self.producer_client.get_task(task_id).error)
 
-        producer_queue.delete_task(producer_client.get_task(task_id))
+        self.producer_queue.delete_task(self.producer_client.get_task(task_id))
 
-        self.assertIsNone(producer_client.get_task(task_id).status)
+        self.assertIsNone(self.producer_client.get_task(task_id).status)
+
+    def test_task_can_return_failing_status(self):
+        task_id = self.producer_queue.enqueue("PARAMETERS")
+
+        task = self.worker_listener.listen()
+
+        processor = self.worker_client.get_processor(task)
+
+        processor.start(123456)
+
+        processor.fail("ERROR")
+
+        self.assertEqual("failed", self.producer_client.get_task(task_id).status)
+        self.assertEqual("ERROR", self.producer_client.get_task(task_id).error)
+        self.assertIsNone(self.producer_client.get_task(task_id).result)
+
+        self.producer_queue.delete_task(self.producer_client.get_task(task_id))
